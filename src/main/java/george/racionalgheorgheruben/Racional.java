12a993777a020931c79c8d2018020c340a09534f
Gheorghe <georgebucu96@gmail.com>
2020-12-10 12:39:35 +0100

    Añade los atributos y el constructor por defecto

--- src/main/java/george/racionalgheorgheruben/Racional.java
+++ src/main/java/george/racionalgheorgheruben/Racional.java
@@ -5,5 +5,13 @@ package george.racionalgheorgheruben;
  * @author Jorge
  */
 public class Racional {
-    
+
+    private int a;
+    private int b;
+
+    public Racional() {
+        this.a = 1;
+        this.b = 1;
+    }
+
 }
