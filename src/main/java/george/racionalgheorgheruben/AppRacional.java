ea8dbcda1a0095c019ece4f69f7ef9f9c34df8f9
Gheorghe <georgebucu96@gmail.com>
2020-12-10 18:36:40 +0100

    Añade dos objetos con sus valores correspondientes

--- src/main/java/george/racionalgheorgheruben/AppRacional.java
+++ src/main/java/george/racionalgheorgheruben/AppRacional.java
@@ -8,6 +8,8 @@ public class AppRacional {
 
     public static void main(String[] args) {
 
-        
+        // Creamos dos objetos Racional con sus valores correspondientes
+        Racional r1 = new Racional(4, 5);
+        Racional r2 = new Racional(3, 4);
     }
 }
