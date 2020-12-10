ec609145e35987d4f61743022bdaa9a370b006ce
RubenVS <rub3nvs97@gmail.com>
2020-12-10 18:48:37 +0100

    Imprimimos los 3 racionales

--- src/main/java/george/racionalgheorgheruben/AppRacional.java
+++ src/main/java/george/racionalgheorgheruben/AppRacional.java
@@ -11,8 +11,12 @@ public class AppRacional {
         // Creamos dos objetos Racional con sus valores correspondientes
         Racional r1 = new Racional(4, 5);
         Racional r2 = new Racional(3, 4);
-        
+
         //Creamos el Racional con variables aleatorias
-        racional.aleatorio();
+        Racional.aleatorio();
+        
+        r1.imprimirConsola();
+        r2.imprimirConsola();
+        System.out.println(Racional.aleatorio());
     }
 }
